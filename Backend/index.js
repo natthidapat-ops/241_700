de1467b7cf356db7f0bfa69cd6b5e8f189a6837b
natthidapat-ops <natthida.pat@ku.th>
2026-02-27 13:07:11 +0700

    Add files via upload

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,93 @@
 const express =  require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2/promise');
 const app = express();
 
 const port = 8000 ;
+
+const initMySQL = async () => {
+    conn = await mysql.createConnection({
+         host: 'localhost',
+            user: 'root',
+            password: 'root',
+            database: 'webdb',
+            port: 8700
+    });
+    console.log('Connected to MYSQL database');
+}
+
+app.get('/testdb-new', async (req, res) => {
+    try {
+
+        const results = await conn.query('SELECT * FROM users');
+        res.json(results[0]);
+
+    } catch (err) {
+        console.error('Error connecting to the database:' , err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+//path: Get /users สำหรับดึงข้อมูล users ทั้งหมด
+app.get('/users', async(req,res)=>{
+    const results = await conn.query('SELECT * FROM users');
+    res.json(results[0]);
+})
+//path: POST /ueses สำหรับเพื่ม user ใหม่
+app.post('/users',async(req,res)=>{
+    try{
+        let user = req.body;
+    const results = await conn.query('INSERT INTO users SET ?',user);
+    res.json({
+        massage: 'User added successfully',
+        data: results[0]
+    });
+
+    }catch(error){
+        console.error('Error inserting user:',error);
+        res.status(500).json({message: 'Erorr adding user'});
+    }
+})
+//path: = GET / users/:id สำหรัลดึงข้อมู, user ตาม id
+    app.get('/users/:id',async(req,res)=> {
+        try{
+            let id = req.params.id;
+            const results = await conn.query('SELECT * FROM users WHERE id = ?',id);
+            if(results[0].length === 0){
+                throw{ statusCode: 404, message: 'User not found'};
+            }
+            res.json(results[0][0]);
+        }catch(error){
+            console.error('Error fetching user:',error);
+            let statusCode = error.statusCode || 500;
+            res.status(statusCode).json({
+                massage: error.massage || 'Error fetching user'
+            });
+        }
+    })
+
+//path: = PUT /users/:id สำหรับอัพเดตข้อมูล User ตาม id
+app.put('/users/:id',async(req,res) =>{
+    let id = req.params.id;
+    let updatedUser = req.body;
+    const results = await conn.query('UPDATE users SET ? WHERE id = ?', id);
+    res.json({
+        message: 'User deleted successfully',
+        data: results[0]
+    });
+
+
+})
+
+/**     const conn = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password:   'root',
+        database: 'webdb',
+        port:8700
+    });
+    const results = await conn.query('SELECT * FROM users');
+    res.json(results[0]);
 
 app.use(bodyParser.json());
 
@@ -83,6 +168,7 @@ app.delete('/users/:id', (req, res)=>{
     });
 });
   
-app.listen(port, () => {
+app.listen(port, async() => {
+    await initMySQL();
     console.log(`Server is running on http://localhost:${port}`);
 });
